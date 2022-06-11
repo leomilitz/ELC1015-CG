cc6287fa96bbb1b17ef15aecea6c32877f061b73
Leonardo Militz <39914866+leomilitz@users.noreply.github.com>
2022-06-11 17:12:26 -0300

    Overgrown Grass added

--- Trab 3 lamilitz/src/Scenery/Grass.cpp
+++ Trab 3 lamilitz/src/Scenery/Grass.cpp
@@ -7,11 +7,24 @@ Grass::Grass(Vector2 pos, float width, float height, float r, float g, float b, 
    this->speed = speed;
    this->speedMultiplier = speedMultiplier;
    this->r = r; this->g = g; this->b = b;
+
+   hasOvergrownGrass = false;
+   if (ru.getRandomInt(0, 100) <= 20)
+      hasOvergrownGrass = true;
 }
 
 void Grass::render(float fps) {
    CV::color(r,g,b);
    CV::rectFill(pos, Vector2(pos.x + width, pos.y - height));
+
+   if (hasOvergrownGrass) {
+      CV::line(pos.x + width*0.5, pos.y, pos.x + width*0.2, pos.y + height*0.8, 3);
+      CV::line(pos.x + width*0.1, pos.y, pos.x + width, pos.y + height*0.5, 3);
+      CV::line(pos.x + width*0.7, pos.y, pos.x + width*1.4, pos.y + height*0.4, 3);
+      CV::line(pos.x, pos.y, pos.x - width*0.1, pos.y + height*0.3, 2);
+      CV::line(pos.x + width*0.5, pos.y, pos.x + width*0.5, pos.y + height*0.4, 2);
+   }
+
    pos.x -= speed*speedMultiplier/fps;
 }
 
--- Trab 3 lamilitz/src/Scenery/Grass.h
+++ Trab 3 lamilitz/src/Scenery/Grass.h
@@ -3,13 +3,15 @@
    Autor: Leonardo Militz
 
    Classe para renderizar uma parcela de grama no cenário. Seu tamanho e cor é gerada
-   aleatóriamente na classe Background.
+   aleatóriamente na classe Background. Existe 20% de chance de spawnar uma grama com
+   aparencia de "crescida".
 */
 
 #ifndef GRASS_H
 #define GRASS_H
 
 #include "../gl_canvas2d.h"
+#include "../RandomUtils.h"
 
 class Grass {
    public:
@@ -22,6 +24,9 @@ class Grass {
       Vector2 pos;
    private:
       float speed, speedMultiplier, width, height, r, g, b;
+      bool hasOvergrownGrass;
+
+      RandomUtils ru;
 };
 
 #endif // GRASS_H

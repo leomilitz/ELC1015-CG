754bff248fb2c7d24c2988eec1a4e4c746be3f3c
Leonardo Militz <39914866+leomilitz@users.noreply.github.com>
2022-06-13 15:22:08 -0300

    Update spokes rendering

--- Trab 3 lamilitz/src/Player/Wheel.cpp
+++ Trab 3 lamilitz/src/Player/Wheel.cpp
@@ -13,15 +13,15 @@ Wheel::Wheel(int radius, int centerX, int centerY, int tireThickness) {
 void Wheel::drawSpokes() {
    CV::color(0.7,0.7,0.7);
 
-   for (float i=0; i <= 1.75; i+=0.25)
-      CV::line(0, 0, cos(PI*i + angle)*radius, sin(PI*i + angle)*radius, 4);
+   CV::translate(centerX, centerY);
+   for (float i=0; i <= 2.0; i+=0.2)
+      CV::line(0, 0, cos(PI*i + angle)*radius, sin(PI*i + angle)*radius, 3);
 
    angle -= speed*speedMultiplier/fps;
 }
 
 void Wheel::render(float fps) {
    this->fps = fps;
-   CV::translate(centerX, centerY);
    drawSpokes();
    CV::color(0,0,0);
    CV::circle(0, 0, radius, 25, 10);
